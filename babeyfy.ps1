f059e4bfdc9b9ccde2d7be4a1d00e099085baa13
Jake McLellan <jake2628@gmail.com>
2020-04-26 20:22:08 -0400

    Update babeyfy.ps1

--- babeyfy.ps1
+++ babeyfy.ps1
@@ -1,4 +1,4 @@
-wget -O background.jpg https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/s960x960/93885248_1481637268682572_6924295563932336128_o.jpg?_nc_cat=108&_nc_sid=cdbe9c&_nc_ohc=YO8iBAEDJCoAX9_CWEf&_nc_ht=scontent.fewr1-1.fna&_nc_tp=7&oh=27aa02290195c9fccabb1e94d6fb88ba&oe=5EC8C132
+wget -O background.jpg "https://scontent.fewr1-1.fna.fbcdn.net/v/t1.0-9/s960x960/93885248_1481637268682572_6924295563932336128_o.jpg?_nc_cat=108&_nc_sid=cdbe9c&_nc_ohc=YO8iBAEDJCoAX9_CWEf&_nc_ht=scontent.fewr1-1.fna&_nc_tp=7&oh=27aa02290195c9fccabb1e94d6fb88ba&oe=5EC8C132"
 $dir = Get-Location
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WallPaper" -value "$dir/background.jpg"
 
